c70d7e62c91f62de448078a923b92a7a745e510e
Bruno Alexandre Rosa <1791393+brunoalr@users.noreply.github.com>
2026-03-14 12:05:13 -0300

    dummy cpp change

--- cockatrice/src/main.cpp
+++ cockatrice/src/main.cpp
@@ -185,6 +185,7 @@ int main(int argc, char *argv[])
     const QByteArray configPath = "";
 #endif
 
+    std::cout << "configPath: " << configPath.constData() << std::endl;
     if (!qEnvironmentVariableIsSet(("QT_LOGGING_CONF"))) {
         // Set the QT_LOGGING_CONF environment variable
         qputenv("QT_LOGGING_CONF", configPath);
